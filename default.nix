velentr/buildroot.nix

commit 0e58b98bbca70561bb20724b60afd828afafb4af

default.nix: packageInputs: Initial revision
Buildroot requires a bunch of source tarballs that it will download
from the internet if they don't alreay exist. Have nix pre-download
these since the builder cannot access the internet during the actual
build.

This definition is generated from a lockfile, which can be built for
the given defconfig/source tree using the existing packageLockFile
derivation. This can't be generated dynamically since nix is a
non-monadic build system. Instead, the lock file should be built and
checked into VCS, then passed as an input to the build. A future check
will ensure that the lockfile is up-to-date.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

default.nix
@@ -44,4 +44,25 @@ in rec {
     '';
     dontInstall = true;
   };
+
+  packageInputs = {lockfile}: let
+    lockedInputs = builtins.fromJSON (builtins.readFile lockfile);
+    symlinkCommands = builtins.map (
+      file: let
+        lockedAttrs = lockedInputs.${file};
+        input = pkgs.fetchurl {
+          name = file;
+          urls = lockedInputs.${file}.uris;
+          hash = "${lockedAttrs.algo}:${lockedAttrs.checksum}";
+        };
+      in "ln -s ${input} $out/${file}"
+    ) (builtins.attrNames lockedInputs);
+  in
+    stdenv.mkDerivation {
+      name = "${defconfig}-sources";
+      dontUnpack = true;
+      dontConfigure = true;
+      buildPhase = "mkdir $out";
+      installPhase = pkgs.lib.strings.concatStringsSep "\n" symlinkCommands;
+    };
 }

commit 23c7610e224dbf738c7b3bb5d7b4c573ffde624f

default.nix: lockedInputs: Fix files with query parameters
When installing symlinks for the lockedInputs build, files may contain
query parameters including '=', which breaks the symlink command for
that file. This would result in some downloaded outputs (such as
spidev_test.c) not being present in $BR2_DL_DIR, so the build would
fail trying to download it.

Fix this by adding additional quoting. Note that nix fixes this for
the input store path automatically, but not for the file reference.

Fixes: 0e58b98 ("default.nix: packageInputs: Initial revision")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

default.nix
@@ -83,7 +83,7 @@ in rec {
           urls = lockedInputs.${file}.uris;
           hash = "${lockedAttrs.algo}:${lockedAttrs.checksum}";
         };
-      in "ln -s ${input} $out/${file}"
+      in "ln -s ${input} $out/'${file}'"
     ) (builtins.attrNames lockedInputs);
   in
     stdenv.mkDerivation {
@@ -105,7 +105,7 @@ in rec {
           export BR2_DL_DIR=/build/source/downloads
           mkdir $BR2_DL_DIR
           for lockedInput in ${lockedPackageInputs}/*; do
-              ln -s $lockedInput $BR2_DL_DIR/$(basename $lockedInput)
+              ln -s $lockedInput "$BR2_DL_DIR/$(basename $lockedInput)"
           done
 
           ${makeFHSEnv}/bin/make-with-fhs-env