velentr/buildroot.nix

commit 85094e0d84e4baaf8421bb747a44cfad7e4f3dda

default.nix: packageLockFile: Initial revision
Given the package info JSON generated by buildroot, generate a
JSON-formatted lock file that specifies which files need to be
downloaded, which URIs to use, and what the resulting hashes should
be.

I should be able to consume this lock file in nix to create
fixed-output derivations for each input file, as well as a "downloads"
directory to use as an input to the buildroot build.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

make-package-lock.py
@@ -1,0 +1,79 @@
+"""Create a buildroot package lock file based on the given package inputs."""
+
+import argparse
+import dataclasses
+import glob
+import json
+import pathlib
+import re
+import typing as T
+
+
+@dataclasses.dataclass(frozen=True)
+class DownloadInfo:
+    algo: str
+    checksum: str
+
+
+def create_download_info(
+    checksums_index: T.Dict[str, DownloadInfo], package_info: T.Dict
+) -> T.Dict:
+    result = {}
+
+    for package in package_info.values():
+        for download in package.get("downloads", []):
+            source = download["source"]
+            uris = [
+                uri.split("+", maxsplit=1)[-1] + "/" + source
+                for uri in download["uris"]
+            ]
+            download_info = checksums_index[source]
+            result[source] = dict(
+                uris=uris, algo=download_info.algo, checksum=download_info.checksum
+            )
+
+    return result
+
+
+def index_download_checksums() -> T.Dict[str, DownloadInfo]:
+    result = {}
+    hash_pattern = re.compile(r"(\w+)\s+([a-zA-Z0-9]+)\s+(\S+)\s*")
+
+    for hashfile in glob.iglob("**/*.hash", recursive=True):
+        with open(hashfile, mode="r") as hashlines:
+            for hashline in hashlines:
+                match = re.fullmatch(hash_pattern, hashline)
+                if match:
+                    result[match.group(3)] = DownloadInfo(
+                        algo=match.group(1), checksum=match.group(2)
+                    )
+
+    return result
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--input",
+        "-i",
+        type=pathlib.Path,
+        required=True,
+        help="Path to the input package info JSON file.",
+    )
+    parser.add_argument(
+        "--output", "-o", type=pathlib.Path, help="Path to write the output lock file."
+    )
+    args = parser.parse_args()
+
+    package_info = json.loads(args.input.read_text())
+    checksums_index = index_download_checksums()
+    downloads_info = create_download_info(checksums_index, package_info)
+    output = json.dumps(downloads_info, indent=2, sort_keys=True)
+    if args.output:
+        args.output.write_text(output)
+    else:
+        print(output)
+
+
+if __name__ == "__main__":
+    main()

default.nix
@@ -4,7 +4,7 @@
   defconfig,
 }: let
   inherit (pkgs) stdenv;
-in {
+in rec {
   packageInfo = stdenv.mkDerivation {
     name = "${defconfig}-packageinfo.json";
     src = src;
@@ -30,5 +30,18 @@ in {
     installPhase = ''
       cp packageinfo.json $out
     '';
+  };
+
+  packageLockFile = stdenv.mkDerivation {
+    name = "${defconfig}-packages.lock";
+    src = src;
+
+    buildInputs = with pkgs; [python3];
+
+    dontConfigure = true;
+    buildPhase = ''
+      python3 ${./make-package-lock.py} --input ${packageInfo} --output $out
+    '';
+    dontInstall = true;
   };
 }

commit 48da47a5051fbdcde9bc53a3befd2519866fb11e

make-package-lock.py: Filter non-http URIs
Several packages have alternative download methods (such as git for
x264) that don't work with the fetchurl derivation in nix. Filter
these out so that only HTTP (or HTTPS) fetches are used.

Note that this should work for all packages, since buildroot has HTTP
mirrors of all sources; those mirrors will get hit instead of the git
source URI.

Fixes: 85094e0 ("default.nix: packageLockFile: Initial revision")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

make-package-lock.py
@@ -19,6 +19,11 @@ class DownloadInfo:
     checksum: str
 
 
+def is_http_download(uri: str) -> bool:
+    # Note that this should handle both http and https with or without '|urlencode'.
+    return uri.split("+", maxsplit=1)[0].startswith("http")
+
+
 def create_download_info(
     checksums_index: T.Dict[str, DownloadInfo], package_info: T.Dict
 ) -> T.Dict:
@@ -30,6 +35,7 @@ def create_download_info(
             uris = [
                 uri.split("+", maxsplit=1)[-1] + "/" + source
                 for uri in download["uris"]
+                if is_http_download(uri)
             ]
             download_info = checksums_index[source]
             result[source] = dict(